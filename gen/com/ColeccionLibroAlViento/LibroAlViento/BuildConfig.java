04bc19020e9daf11355c3ba4e247a3744c2e7515
snaphuman <fhernandezn@gmail.com>
2013-04-18 12:39:55 -0500

    [app] primer commit

---
+++ gen/com/ColeccionLibroAlViento/LibroAlViento/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ColeccionLibroAlViento.LibroAlViento;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
